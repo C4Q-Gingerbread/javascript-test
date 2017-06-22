8df476b33924836b50e0d2c4e0814a33187cba6d
fattyduck <dison.ruan@yahoo.com>
2017-06-22 16:44:37 -0400

    Passed all available test cases.

--- app/recursion.js
+++ app/recursion.js
@@ -2,7 +2,22 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
+    var allFiles = [];
 
+    (function all(filedata) {
+      var files = filedata.files;
+
+      if (typeof files !== 'undefined') {
+        for (var i = 0; i < files.length; i++) {
+          if (typeof files[i] === 'string') {
+            allFiles.push(files[i]);
+             } else {
+               all(files[i]);
+             }
+           }
+         }
+       })(data)
+    return dirName? allFiles.filter(file => file.includes(dirName)) :  allFiles;
   },
 
   permute: function(arr) {
