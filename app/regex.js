da9c3844f757715b2af39210699f27b4db1d1202
fattyduck <dison.ruan@yahoo.com>
2017-06-22 13:06:13 -0400

    Completed Regex.js

--- app/regex.js
+++ app/regex.js
@@ -2,26 +2,28 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
   containsNumber: function(str) {
-
+    return str.search(/[0-9]/)!==-1
   },
 
   containsRepeatingLetter: function(str) {
-
+    return (/([a-zA-Z]).*?\1/).test(str);
   },
 
   endsWithVowel: function(str) {
-
+    return (/[aeiouAEIOU]$/).test(str);
   },
 
   captureThreeNumbers: function(str) {
-
+    return str.match(/[0-9]{3}/) == null? false : str.match(/[0-9]{3}/)[0];
   },
 
   matchesPattern: function(str) {
-
+    if(str.length !== 12) return false;
+    return str.match(/[0-9]{3}-[0-9]{3}-[0-9]{4}/) !== null;
   },
 
   isUSD: function(str) {
-
+    if(str.search(/[_]/)!==-1) return false;
+    return str.match(/^\$([0-9]{1,3},([0-9]{3},)*[0-9]{3}|[0-9]+)(.[0-9][0-9])?$/) !== null;
   }
 };
