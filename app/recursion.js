38b7632748e2f272935d6bfab8ae33baf209de49
fattyduck <dison.ruan@yahoo.com>
2017-06-22 18:49:00 -0400

    added 3 testcase and completed 2

--- app/recursion.js
+++ app/recursion.js
@@ -21,14 +21,35 @@ exports.recursionAnswers = {
   },
 
   permute: function(arr) {
+    //still haven't figure this one out yet
+    var permutations = [];
+    permutations = permutate([], arr, permutations)
+    return permutations;
 
+    function permutate(prefix, suffix, results){
+      if(suffix.length = 0){
+        results.push(prefix);
+        return results
+      }else{
+        for(var i = 0; i < suffix.length; i++){
+          permutate(prefix+suffix[i], suffix.substring(0,i) + suffix[i+1..suffix.length()]);
+        }
+      }
+    }
   },
 
-  fibonacci: function(n) {
-
+  fibonacci: function(num) {
+    if (num <= 2) return 1;
+    return this.fibonacci(num - 1) + this.fibonacci(num - 2);
   },
 
   validParentheses: function(n) {
-
+    if(!n.includes("(") && !n.includes(")")) return true;
+    if(n.includes("(") && n.includes(")")){
+      n = n.replace("(", "");
+      n = n.replace(")", "");
+      return this.validParentheses(n);
+    }
+    return false;
   }
 };
--- tests/app/recursion.js
+++ tests/app/recursion.js
@@ -48,4 +48,20 @@ describe('recursion', function() {
     expect(result.indexOf('main.js') > -1).to.be.ok;
     expect(result.indexOf('underscore.js') > -1).to.be.ok;
   });
+
+  it('you should be able to find all permutations of an array', function(){
+    expect(recursionAnswers.permute([1,2]).length).to.eql(2);
+    expect(recursionAnswers.permute([1,2,3]).length).to.eql(6)
+  });
+
+  it('you should be able to fibonacci', function(){
+    expect(recursionAnswers.fibonacci(6)).to.eql(8);
+    expect(recursionAnswers.fibonacci(11)).to.eql(89);
+  });
+
+  it('you should be able to valid check for valid parentheses', function(){
+    expect(recursionAnswers.validParentheses("()")).to.eql(true);
+    expect(recursionAnswers.validParentheses("()()))")).to.eql(false);
+  });
+
 });
