52c2548cef4443e85ac61620f6c99ed2d4f9a4e9
fattyduck <dison.ruan@yahoo.com>
2017-06-22 13:27:56 -0400

    Completed Count.js

--- app/count.js
+++ app/count.js
@@ -2,6 +2,16 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    console.log(start)
+    start++;
+    var counting = setInterval(() => {
+      console.log(start++)
+      if(start > end) this.cancel();
+    }, 100);
 
+    this.cancel = function(){
+      clearInterval(counting);
+    }
+    return this;
   }
 };
